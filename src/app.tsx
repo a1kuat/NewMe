ed924aec82bdaa6eb60497979f9d22239fe14ae1
Azamat <alkuat1511@gmail.com>
2023-12-04 18:14:33 +0300

    Removed unnecessery things

--- src/app.tsx
+++ src/app.tsx
@@ -2,22 +2,12 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const HomePage = lazy(() => import('./Pages/HomePage'));
-const HabitsPage = lazy(() => import('./Pages/HabitsPage'));
 
 const App = () => {
   return(
     <Router basename="/newme">
       <Routes>
-        <Route path="/" element={
-          <Suspense fallback={<div>Загрузка...</div>}>
-            <HomePage/>
-          </Suspense>
-        }/>
-        <Route path="/habits" element={
-          <Suspense fallback={<div>Загрузка...</div>}>
-            <HabitsPage/>
-          </Suspense>
-        }/>
+        <Route path="/" element={<HomePage/>}/>
       </Routes>
     </Router>
   )
