3eb4e767bd16b1c330756cabd1d6b7d1eff979b7
Azamat <alkuat1511@gmail.com>
2023-12-09 07:53:08 +0300

    Added LoadingPage

--- src/Pages/LoadingPage/LoadingPage.tsx
+++ src/Pages/LoadingPage/LoadingPage.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Backdrop, CircularProgress } from '@mui/material';
 
 const LoadingPage = () => {
-    return (
-      <div className="loading-page">
-        <div className="loading-component">Loading...</div>
-      </div>
-    );
-  };
+  return (
+    <Backdrop
+      sx={{ 
+        color: '#fff', 
+        zIndex: (theme) => theme.zIndex.drawer + 1, 
+        backgroundColor: 'rgba(0, 0, 0, 0.9)' 
+      }}
+      open={true}
+    >
+      <CircularProgress sx={{ color:"red" }} />
+    </Backdrop>
+  );
+}
   
 export default LoadingPage;
