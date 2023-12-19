73ac0c8a58a8732896aa1087e59ed0dc43644efc
aibek.bakirov <aibek.bakirov99@gmail.com>
2023-12-19 14:56:56 +0300

    update app.tsx and navbar.tsx test

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,89 +1,100 @@
-import React from 'react';
-import { AppBar, Toolbar, IconButton , Box, Button } from '@mui/material';
-import { Link , useNavigate } from 'react-router-dom';
-import logo from '../../Images/logo.png'; 
-import {styled} from '@mui/system';
-const Logo = styled('img')({
-    height: '4%', // Adjust size as needed
-    width: '7%', // Adjust size as needed
-    position: 'fixed',
-    top: '2%',
-    left: '2%',
-  });
+import React from "react";
+import { AppBar, Toolbar, IconButton, Box, Button } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "../../Images/logo.png";
+import { styled } from "@mui/system";
+const Logo = styled("img")({
+  height: "4%", // Adjust size as needed
+  width: "7%", // Adjust size as needed
+  position: "fixed",
+  top: "2%",
+  left: "2%",
+});
 
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <AppBar position='fixed' style={{ background: 'transparent', boxShadow: 'none' }}>
+    <AppBar
+      position="fixed"
+      style={{ background: "transparent", boxShadow: "none" }}
+    >
+      <a href="https://ibb.co/sJhJhnC">
+        <img src="https://i.ibb.co/LhMhMmr/logo.png" alt="logo" />
+      </a>
       <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => navigate('/')}>
-          <Logo src={logo} alt="Marvel logo"/>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={() => navigate("/")}
+        >
+          <Logo src="" alt="Marvel logo" />
         </IconButton>
-        <Box sx={{ marginLeft: 'auto' }}>
-          <Button 
-            color="inherit" 
-            component={Link} 
+        <Box sx={{ marginLeft: "auto" }}>
+          <Button
+            color="inherit"
+            component={Link}
             to="/"
             sx={{
-              color: 'white',
-              textDecoration: 'none',
-              fontSize: '20px',
-              padding: '10px 20px',
-              marginRight: '10px',
-              '&:hover': {
-                textDecoration: 'underline',
-                color: 'red',
+              color: "white",
+              textDecoration: "none",
+              fontSize: "20px",
+              padding: "10px 20px",
+              marginRight: "10px",
+              "&:hover": {
+                textDecoration: "underline",
+                color: "red",
               },
             }}
           >
             Home
           </Button>
-          <Button 
-            color="inherit" 
-            component={Link} 
+          <Button
+            color="inherit"
+            component={Link}
             to="/characters"
             sx={{
-              color: 'white',
-              textDecoration: 'none',
-              fontSize: '20px',
-              padding: '10px 20px',
-              '&:hover': {
-                textDecoration: 'underline',
-                color: 'red',
+              color: "white",
+              textDecoration: "none",
+              fontSize: "20px",
+              padding: "10px 20px",
+              "&:hover": {
+                textDecoration: "underline",
+                color: "red",
               },
             }}
           >
             Characters
           </Button>
-          <Button 
-            color="inherit" 
-            component={Link} 
+          <Button
+            color="inherit"
+            component={Link}
             to="/series"
             sx={{
-              color: 'white',
-              textDecoration: 'none',
-              fontSize: '20px',
-              padding: '10px 20px',
-              '&:hover': {
-                textDecoration: 'underline',
-                color: 'red',
+              color: "white",
+              textDecoration: "none",
+              fontSize: "20px",
+              padding: "10px 20px",
+              "&:hover": {
+                textDecoration: "underline",
+                color: "red",
               },
             }}
           >
             Series
           </Button>
-          <Button 
-            color="inherit" 
-            component={Link} 
+          <Button
+            color="inherit"
+            component={Link}
             to="/graph"
             sx={{
-              color: 'white',
-              textDecoration: 'none',
-              fontSize: '20px',
-              padding: '10px 20px',
-              '&:hover': {
-                textDecoration: 'underline',
-                color: 'red',
+              color: "white",
+              textDecoration: "none",
+              fontSize: "20px",
+              padding: "10px 20px",
+              "&:hover": {
+                textDecoration: "underline",
+                color: "red",
               },
             }}
           >
--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route , Link } from 'react-router-dom';
-import SeriesPage from './Pages/SeriesPage';
-import SeriesDetailPage from './Pages/SeriesDetailPage';
-import CharactersDetailPage from './Pages/CharactersDetailPage';
-import HomePage from './Pages/HomePage';
-import CharactersPage from './Pages/CharactersPage';
-import Navbar from './Components/Navbar/Navbar';
-import GraphPage from './Pages/GraphPage/GraphPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SeriesPage from "./Pages/SeriesPage";
+import SeriesDetailPage from "./Pages/SeriesDetailPage";
+import CharactersDetailPage from "./Pages/CharactersDetailPage";
+import HomePage from "./Pages/HomePage";
+import CharactersPage from "./Pages/CharactersPage";
+import Navbar from "./Components/Navbar/Navbar";
+import GraphPage from "./Pages/GraphPage/GraphPage";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const App = () => {
-  return(
-    <Router basename="/NewMe">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/series" element={<SeriesPage/>}/>
-        <Route path="/series/:id" element={<SeriesDetailPage/>}/>
-        <Route path="/characters" element={<CharactersPage/>}/>
-        <Route path="/characters/:id" element={<CharactersDetailPage/>}/>
-        <Route path="/graph" element={<GraphPage/>}/>
-      </Routes>
-    </Router>
-  )
-}
+  return (
+    <div className="container main-content">
+      <Router basename="/NewMe">
+        <Navbar />
+        <a href="https://ibb.co/sJhJhnC">
+          <img src="https://i.ibb.co/LhMhMmr/logo.png" alt="logo" />
+        </a>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/series" element={<SeriesPage />} />
+          <Route path="/series/:id" element={<SeriesDetailPage />} />
+          <Route path="/characters" element={<CharactersPage />} />
+          <Route path="/characters/:id" element={<CharactersDetailPage />} />
+          <Route path="/graph" element={<GraphPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
