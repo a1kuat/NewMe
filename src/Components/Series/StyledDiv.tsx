c24cd2c487a4f9ab1493f1f8f8ca8e285e2da843
aibek.bakirov <aibek.bakirov99@gmail.com>
2023-12-19 18:15:51 +0300

    update background images were corrected home page, characters page, series page

--- src/Components/Series/StyledDiv.tsx
+++ src/Components/Series/StyledDiv.tsx
@@ -1,15 +1,15 @@
-import {styled} from '@mui/system';
-import seriesImage from '../../Images/spiderman.jpg';
+import { styled } from "@mui/system";
+import seriesImage from "../../Images/spiderman.jpg";
 
-const StyledDiv = styled('div')({
-    //backgroundImage: `url(${seriesImage})`,
-    backgroundColor: 'black',
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    width: '100vw',
-    height: '100vh',
-    overflow: 'auto'
-  });
+const StyledDiv = styled("div")({
+  backgroundImage: `url("https://i.postimg.cc/XN23DCjf/spiderman.jpg")`,
+  backgroundColor: "black",
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  width: "100vw",
+  height: "100vh",
+  overflow: "auto",
+});
 
 export default StyledDiv;
